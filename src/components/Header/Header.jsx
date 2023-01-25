a0010c13e4f0d69b911a5fce21e5b222dbddb028
em1ldv <emildv_dev@mail.ru>
2023-01-26 04:34:55 +0500

    modified:   src/components/Button/Button.css modified:   src/components/Header/Header.css modified:   src/components/Header/Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useTelegram } from "../../hooks/useTelegram";
 import Button from "../Button/Button";
+import "./Header.css";
 
-const Header = (props) => {
+const Header = () => {
     
     const {tg, user, onClose} = useTelegram();
    
-
     return (
         <div className={'header'}>
             <Button onClick={onClose} >Закрыть</Button>
             <span className={'username'}>
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </span>
         </div>
     )

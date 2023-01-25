bbdef94bc4a2b83a5c4484608a7056a3caf3d2f3
em1ldv <emildv_dev@mail.ru>
2023-01-26 04:16:13 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import { useEffect } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import { useTelegram } from './hooks/useTelegram';
 
-const tg = window.Telegram.WebApp;
 
 function App() {
   
-  // useEffect(() => {
-  //   tg.Ready();
-  // }, [])
-
-  const OnClose = () => {
-    tg.close();
-  }
+  const {onToggleButton, tg} = useTelegram() 
+ 
 
 
   return (
     <div className="App">
-        test
-
-        <button onClick={OnClose}>Закрыть</button>
+        <Header />
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+import Button from "../Button/Button";
+
+const Header = (props) => {
+    
+    const {tg, user, onClose} = useTelegram();
+   
+
+    return (
+        <div className={'header'}>
+            <Button>Закрыть</Button>
+            <span className={'username'}>
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    )
+}
+
+export default Header
